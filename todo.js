8e8603bea9b161d55569ec86a6301288f3f836de
ceres1105 <59174539+ceres1105@users.noreply.github.com>
2020-01-02 22:33:02 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -4,16 +4,43 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+/*cleanToDos , filter 가 하는 일은 filterFN체크가 된
+아이템들을 array로 만들어 주는 것*/
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+} /*js는 object 를 string으로 저장하려고 그래서 JSON 사용한대
+JSON은 JavaScript Object Notation 줄임말*/
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
-  li.appendChild(delBtn);
-  //btn버튼을 li안에 넣은것
+  li.appendChild(delBtn); /*btn버튼을 li안에 넣는다는 뜻*/
+  li.id = newId;  /*이거 괄호 안하는듯 */
   li.appendChild(span);
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos(); /*push한 이후에 호 전에하면 todo에 아무것도 없 */
 }
 
 function handleSubmit(event) {
@@ -24,10 +51,15 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+      const parsedToDos = JSON.parse(loadedToDos);
+      parsedToDos.forEach(function(toDo) {
+        paintToDo(toDo.text);
+      });
   }
 }
+
 
 function init() {
   loadToDos();
